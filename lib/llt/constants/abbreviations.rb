ac041fb9c362fbd1cf7f1d455e2ceb78f81cb54e
lichtr <robert.lichtensteiner@gmail.com>
2014-08-27 22:42:32 +0200

    Update abbreviations

--- lib/llt/constants/abbreviations.rb
+++ lib/llt/constants/abbreviations.rb
@@ -4,10 +4,13 @@ module LLT
       NAMES = %w{ A D C K L M N P Q S T App Ap Cn Mam M' Ser Sex Sp Tib Ti }
       NAMES_PIPED = NAMES.join('|')
 
-      DATES = %w{ a d Kal Non Id Ian Feb Mar Apr Mai Iun Iul Aug Sep Oct Nov Dec }
+      DATES = %w{ a d Kal Non Id Ian Feb Mar Apr Mai Iun Iul Aug Sep Oct Octob Nov Dec }
       DATES_PIPED = DATES.join('|')
 
-      ALL_ABBRS_PIPED = "#{NAMES_PIPED}|#{DATES_PIPED}"
+      COS = %w{ Cos Coss Conss Cons cos coss conss cons }
+      COS_PIPED = COS.join('|')
+
+      ALL_ABBRS_PIPED = "#{NAMES_PIPED}|#{DATES_PIPED}|#{COS_PIPED}"
 
       APOSTROPHES = %w{ po min vin potin satin scin tun }
       APOSTROPHES_PIPED = APOSTROPHES.join('|')
